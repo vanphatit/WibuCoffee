dc868fa531cc98a4043f7dc6b286ab58663ae343
vanphat15it <vanphat15it@outlook.com>
2024-04-10 00:57:11 +0700

    Added navigation bar; user controls, and fix bugs

--- View/MainWindow.Designer.cs
+++ View/MainWindow.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnManage = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnClose = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.btnDashboard = new System.Windows.Forms.Button();
-            this.btnManage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel2
@@ -59,16 +59,40 @@
             this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
             this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseUp);
             // 
-            // label1
+            // panel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Google Sans", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(126, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 30);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "WIBU COFFEE";
+            this.panel1.Controls.Add(this.btnManage);
+            this.panel1.Controls.Add(this.btnDashboard);
+            this.panel1.Location = new System.Drawing.Point(561, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(380, 60);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnManage
+            // 
+            this.btnManage.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnManage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManage.ForeColor = System.Drawing.Color.White;
+            this.btnManage.Location = new System.Drawing.Point(190, 0);
+            this.btnManage.Name = "btnManage";
+            this.btnManage.Size = new System.Drawing.Size(190, 60);
+            this.btnManage.TabIndex = 3;
+            this.btnManage.Text = "MANAGE";
+            this.btnManage.UseVisualStyleBackColor = false;
+            this.btnManage.Click += new System.EventHandler(this.btnManage_Click);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnDashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.ForeColor = System.Drawing.Color.White;
+            this.btnDashboard.Location = new System.Drawing.Point(0, 0);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(190, 60);
+            this.btnDashboard.TabIndex = 2;
+            this.btnDashboard.Text = "DASHBOARD";
+            this.btnDashboard.UseVisualStyleBackColor = false;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
             // 
             // pictureBox1
             // 
@@ -96,46 +120,23 @@
             this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // panel1
+            // label1
             // 
-            this.panel1.Controls.Add(this.btnManage);
-            this.panel1.Controls.Add(this.btnDashboard);
-            this.panel1.Location = new System.Drawing.Point(561, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(380, 60);
-            this.panel1.TabIndex = 2;
-            // 
-            // btnDashboard
-            // 
-            this.btnDashboard.BackColor = System.Drawing.Color.DodgerBlue;
-            this.btnDashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDashboard.ForeColor = System.Drawing.Color.White;
-            this.btnDashboard.Location = new System.Drawing.Point(0, 0);
-            this.btnDashboard.Name = "btnDashboard";
-            this.btnDashboard.Size = new System.Drawing.Size(190, 60);
-            this.btnDashboard.TabIndex = 2;
-            this.btnDashboard.Text = "DASHBOARD";
-            this.btnDashboard.UseVisualStyleBackColor = false;
-            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
-            // 
-            // btnManage
-            // 
-            this.btnManage.BackColor = System.Drawing.Color.DodgerBlue;
-            this.btnManage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnManage.ForeColor = System.Drawing.Color.White;
-            this.btnManage.Location = new System.Drawing.Point(190, 0);
-            this.btnManage.Name = "btnManage";
-            this.btnManage.Size = new System.Drawing.Size(190, 60);
-            this.btnManage.TabIndex = 3;
-            this.btnManage.Text = "MANAGE";
-            this.btnManage.UseVisualStyleBackColor = false;
-            this.btnManage.Click += new System.EventHandler(this.btnManage_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Google Sans", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(126, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "WIBU COFFEE";
             // 
             // panel3
             // 
-            this.panel3.Location = new System.Drawing.Point(3, 65);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 62);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1499, 778);
+            this.panel3.Size = new System.Drawing.Size(1502, 793);
             this.panel3.TabIndex = 2;
             // 
             // MainWindow
@@ -151,8 +152,8 @@
             this.Text = "MainWindow";
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
--- View/UC/UCManage.Designer.cs
+++ View/UC/UCManage.Designer.cs
@@ -28,32 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.btnCustomer = new System.Windows.Forms.Button();
+            this.btnEmployee = new System.Windows.Forms.Button();
+            this.btnProduct = new System.Windows.Forms.Button();
+            this.btnMaterial = new System.Windows.Forms.Button();
+            this.btnReceiptNote = new System.Windows.Forms.Button();
+            this.btnSupplier = new System.Windows.Forms.Button();
+            this.btnExpenseBill = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // panel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(727, 381);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Manage";
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1499, 42);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnExpenseBill);
+            this.panel2.Controls.Add(this.btnSupplier);
+            this.panel2.Controls.Add(this.btnReceiptNote);
+            this.panel2.Controls.Add(this.btnMaterial);
+            this.panel2.Controls.Add(this.btnProduct);
+            this.panel2.Controls.Add(this.btnEmployee);
+            this.panel2.Controls.Add(this.btnCustomer);
+            this.panel2.Controls.Add(this.btnOrder);
+            this.panel2.Location = new System.Drawing.Point(57, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1385, 42);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Font = new System.Drawing.Font("Google Sans", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrder.Location = new System.Drawing.Point(0, 0);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(177, 42);
+            this.btnOrder.TabIndex = 2;
+            this.btnOrder.Text = "ORDER";
+            this.btnOrder.UseVisualStyleBackColor = true;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // btnCustomer
+            // 
+            this.btnCustomer.Font = new System.Drawing.Font("Google Sans", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCustomer.Location = new System.Drawing.Point(347, 0);
+            this.btnCustomer.Name = "btnCustomer";
+            this.btnCustomer.Size = new System.Drawing.Size(177, 42);
+            this.btnCustomer.TabIndex = 3;
+            this.btnCustomer.Text = "CUSTOMER";
+            this.btnCustomer.UseVisualStyleBackColor = true;
+            this.btnCustomer.Click += new System.EventHandler(this.btnCustomer_Click);
+            // 
+            // btnEmployee
+            // 
+            this.btnEmployee.Font = new System.Drawing.Font("Google Sans", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployee.Location = new System.Drawing.Point(174, 0);
+            this.btnEmployee.Name = "btnEmployee";
+            this.btnEmployee.Size = new System.Drawing.Size(177, 42);
+            this.btnEmployee.TabIndex = 4;
+            this.btnEmployee.Text = "EMPLOYEE";
+            this.btnEmployee.UseVisualStyleBackColor = true;
+            this.btnEmployee.Click += new System.EventHandler(this.btnEmployee_Click);
+            // 
+            // btnProduct
+            // 
+            this.btnProduct.Font = new System.Drawing.Font("Google Sans", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProduct.Location = new System.Drawing.Point(521, 0);
+            this.btnProduct.Name = "btnProduct";
+            this.btnProduct.Size = new System.Drawing.Size(177, 42);
+            this.btnProduct.TabIndex = 5;
+            this.btnProduct.Text = "PRODUCT";
+            this.btnProduct.UseVisualStyleBackColor = true;
+            this.btnProduct.Click += new System.EventHandler(this.btnProduct_Click);
+            // 
+            // btnMaterial
+            // 
+            this.btnMaterial.Font = new System.Drawing.Font("Google Sans", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaterial.Location = new System.Drawing.Point(689, 0);
+            this.btnMaterial.Name = "btnMaterial";
+            this.btnMaterial.Size = new System.Drawing.Size(177, 42);
+            this.btnMaterial.TabIndex = 6;
+            this.btnMaterial.Text = "MATERIAL";
+            this.btnMaterial.UseVisualStyleBackColor = true;
+            this.btnMaterial.Click += new System.EventHandler(this.btnMaterial_Click);
+            // 
+            // btnReceiptNote
+            // 
+            this.btnReceiptNote.Font = new System.Drawing.Font("Google Sans", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReceiptNote.Location = new System.Drawing.Point(861, 0);
+            this.btnReceiptNote.Name = "btnReceiptNote";
+            this.btnReceiptNote.Size = new System.Drawing.Size(177, 42);
+            this.btnReceiptNote.TabIndex = 7;
+            this.btnReceiptNote.Text = "RECEIPT NOTE";
+            this.btnReceiptNote.UseVisualStyleBackColor = true;
+            this.btnReceiptNote.Click += new System.EventHandler(this.btnReceiptNote_Click);
+            // 
+            // btnSupplier
+            // 
+            this.btnSupplier.Font = new System.Drawing.Font("Google Sans", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSupplier.Location = new System.Drawing.Point(1035, 0);
+            this.btnSupplier.Name = "btnSupplier";
+            this.btnSupplier.Size = new System.Drawing.Size(177, 42);
+            this.btnSupplier.TabIndex = 8;
+            this.btnSupplier.Text = "SUPPLIER";
+            this.btnSupplier.UseVisualStyleBackColor = true;
+            this.btnSupplier.Click += new System.EventHandler(this.btnSupplier_Click);
+            // 
+            // btnExpenseBill
+            // 
+            this.btnExpenseBill.Font = new System.Drawing.Font("Google Sans", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExpenseBill.Location = new System.Drawing.Point(1209, 0);
+            this.btnExpenseBill.Name = "btnExpenseBill";
+            this.btnExpenseBill.Size = new System.Drawing.Size(177, 42);
+            this.btnExpenseBill.TabIndex = 9;
+            this.btnExpenseBill.Text = "EXPENSE BILL";
+            this.btnExpenseBill.UseVisualStyleBackColor = true;
+            this.btnExpenseBill.Click += new System.EventHandler(this.btnExpenseBill_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(0, 48);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1408, 446);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 48);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1499, 730);
+            this.panel4.TabIndex = 2;
             // 
             // UCManage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
             this.Name = "UCManage";
             this.Size = new System.Drawing.Size(1499, 778);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Button btnExpenseBill;
+        private System.Windows.Forms.Button btnSupplier;
+        private System.Windows.Forms.Button btnReceiptNote;
+        private System.Windows.Forms.Button btnMaterial;
+        private System.Windows.Forms.Button btnProduct;
+        private System.Windows.Forms.Button btnEmployee;
+        private System.Windows.Forms.Button btnCustomer;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
     }
 }
